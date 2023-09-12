557bfe031ad17dace2f08df9d1ce3a48b132f761
Edahí Antonio Gutiérrez Reyes <edahigure@gmail.com>
2023-09-12 13:39:20 -0600

    Add category page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -20,6 +20,9 @@ class CategoriesController < ApplicationController
   # POST /categories or /categories.json
   def create
     @category = Category.new(category_params)
+    @category.user_id = current_user.id
+
+    flash[:alert] = "Here ok. "
 
     respond_to do |format|
       if @category.save
