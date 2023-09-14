8ca45ede65b84fecad24be1afe57d8358822385c
Edahí Antonio Gutiérrez Reyes <edahigure@gmail.com>
2023-09-14 13:57:08 -0600

    Add welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   resources :categories do
     resources :expenses
   end
 
-  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "categories#index"
+  #root "categories#index"
+  
+  get '/sessions/index', to: 'devise/sessions#index', as: :sessions_index
+  
+  devise_scope :user do
+     root "devise/sessions#index"
+  end
 end
